92109c55423a2d3d268fbc258f8a25ec9dbd4fc0
luanfsouza <luanferreira7777@gmail.com>
2024-08-27 20:44:45 -0300

    introducao ao angular

---
+++ introducao-angular/todo/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+
+}
+ 
